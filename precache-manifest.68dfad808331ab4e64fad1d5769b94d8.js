9b7a2108825452262ff4be09a2871e380f2d1b4b
cw-infinite <deadmeats@hotmail.com>
2020-04-14 19:32:48 -0400

    Updates

---
+++ precache-manifest.68dfad808331ab4e64fad1d5769b94d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce9d817afee4e6f525beffc1aba4677",
+    "url": "/bemonster/index.html"
+  },
+  {
+    "revision": "5a312fca73c7fda1c985",
+    "url": "/bemonster/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07e3bb89bb0d50ff2f10",
+    "url": "/bemonster/static/js/2.ea83cbf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bemonster/static/js/2.ea83cbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a312fca73c7fda1c985",
+    "url": "/bemonster/static/js/main.2373546f.chunk.js"
+  },
+  {
+    "revision": "ccb2ebdf852e4193b1fb",
+    "url": "/bemonster/static/js/runtime-main.1ea2fe60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bemonster/static/media/logo.5d5d9eef.svg"
+  }
+]);
