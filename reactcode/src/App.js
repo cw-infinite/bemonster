f74ccb997c8661d4a27913b02cc5437c7742e457
cw-infinite <deadmeats@hotmail.com>
2020-04-14 19:37:52 -0400

    첫 리액트

---
+++ reactcode/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world this is playground for @cw-infinite and @jsong0521
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/cw-infinite/bemonster"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Come visit our Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
